6c0996a14fe8e0a19f4eab3cc7383a1dc6bf3c09
JUJU-Arch <juju.jetstream@gmail.com>
2018-03-11 00:21:58 +0900

    working django program

--- Django/src/OthloBlog/settings.py
+++ Django/src/OthloBlog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'OthloBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
