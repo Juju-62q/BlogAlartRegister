b04ecb1f1841068e4aaf7f8243cd99639d85ab85
JUJU-Arch <juju.jetstream@gmail.com>
2018-03-10 22:20:12 +0900

    make google auth

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main(){
+import (
+	"github.com/Juju-62q/BlogAlartRegister/g_calendar"
+	"context"
+	"io/ioutil"
+	"log"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"fmt"
+)
 
+func main(){
+	ctx := context.Background()
+
+	authData, err := ioutil.ReadFile("/home/kenya/client_secret.json")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	config, err := google.ConfigFromJSON(authData, calendar.CalendarReadonlyScope)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+
+	client := g_calendar.GetClient(ctx, config)
+	if client != nil{
+		fmt.Println("success")
+	}else{
+		fmt.Println("failed")
+	}
 }
