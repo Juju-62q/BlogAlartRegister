b844661cdd10736d8fbdd2a5d9060dc217062bcc
JUJU-Arch <juju.jetstream@gmail.com>
2018-04-01 23:43:49 +0900

    add event to calendar

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 
 	"time"
 
-	"encoding/json"
-	"fmt"
-
 	"math/rand"
 
 	"github.com/Juju-62q/BlogAlartRegister/db"
+	"github.com/Juju-62q/BlogAlartRegister/g_calendar"
 )
 
+const location = "Asia/Tokyo"
+
 func init() {
+	//set rand
 	rand.Seed(time.Now().UnixNano())
 }
 
@@ -39,12 +40,21 @@ func main() {
 	var allMember []Member
 	database.Find(&allMember)
 	shuffle(allMember)
+
+	timeZone, err := time.LoadLocation(location)
+	if err != err {
+		timeZone = time.FixedZone(location, 9*60*60)
+	}
+
+	date := time.Now()
+	date = time.Date(date.Year(), date.Month(), date.Day(), 20, 0, 0, 0, timeZone)
+	date = nextWeek(date)
 	for _, member := range allMember {
-		bytes, err := json.Marshal(member)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(string(bytes))
+		date = nextWeek(date)date.AddDate(0, 0, 7)
+		gcalendar.AddEvent("ブログ"+member.Name, "OthloBlog", member.SlackName, date, date.Add(1*time.Hour))
 	}
 }
 
@@ -55,4 +65,8 @@ func shuffle(data []Member) {
 		data[i], data[j] = data[j], data[i]
 	}
 	return
+}
+
+func nextWeek(date time.Time) time.Time {
+	return date.AddDate(0, 0, 7)
 }
